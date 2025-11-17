f4afba507119048e7a9d91d1f7db40e62980f463
Ashkan <bidekaniashkan@gmail.com>
2025-11-17 22:14:31 +0330

    I used ChatGPT to complete the task at all

--- main.py
+++ main.py
@@ -1,11 +1,37 @@
-from random import uniform
+import random
+from random import randint
+from datetime import datetime,timedelta
 
 
-def Gen():
-    x = 0
-    for _ in range(5):
-        x = int(uniform(100000,1000000))
-    return x
+otp_data = {
+    "code": None,
+    "created_at": None
+}
 
 
-print(Gen())
+def gen():
+    otp = random.randint(1000000,100000000)
+    otp_data ["code"] = otp
+    otp_data ["created_at"] = datetime.now()
+    return otp_data
+
+def validation(input_code):
+    if otp_data["code"] == None:
+        return "There is no generated password "
+    now = datetime.now()
+    created = otp_data["created_at"]
+
+    if now - created > timedelta(seconds=10):
+        return "Your one-time password has expired. "
+
+    if input_code == otp_data["code"] :
+        return "The password is Correct"
+
+    else:
+        return "The passsword is not correct"
+
+code = gen()
+print("OTP:", code)
+
+user_input = int(input("Enter OTP: "))
+print(validation(user_input))
